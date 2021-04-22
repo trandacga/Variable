bb10820e4797ead7b4db96612187de6e7b448e37
trandacga <gatran1402@gmail.com>
2021-04-22 15:01:38 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,33 @@
+package basic.dev;
+/**
+ * 
+ * @author Admin
+ *
+ */
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Khai báo biến 	
+		 */
+		String firstName = "Phan Văn";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+				
+		System.out.println(" Ho Va ten");
+		System.out.println( firstName + " "+ lastName);
+		System.out.println("Mark Math:" + markMath);
+		System.out.println("Passed: " + passed);
+		
+		
+		//khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+		
+
+	}
+
+}
